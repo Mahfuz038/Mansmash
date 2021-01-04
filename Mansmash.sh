faa4daacf0d5a23915af345e9a0ff0dd76bf7456
Mahfuz038 <76444680+Mahfuz038@users.noreply.github.com>
2021-01-04 13:14:32 -0500

    Update and rename TBomb.sh to Mansmash.sh

--- Mansmash.sh
+++ Mansmash.sh
@@ -27,19 +27,19 @@ banner() {
     clear
     echo -e "\e[1;31m"
     if ! [ -x "$(command -v figlet)" ]; then
-        echo 'Introducing TBomb'
+        echo 'Introducing Mansmash Made by Mahfuz'
     else
         figlet TBomb
     fi
     if ! [ -x "$(command -v toilet)" ]; then
-        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSpeedX \e[0m"
+        echo -e "\e[4;34m This Bomber Was Created By \e[1;32Founder of Dark-Evil MR. Mahfuz Rahman \e[0m"
     else
         echo -e "\e[1;34mCreated By \e[1;34m"
-        toilet -f mono12 -F border SpeedX
+        toilet -f mono12 -F border Dark-Evil
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
-    echo -e "\e[1;32m           Telegram: https://t.me/TBombChat \e[0m"
-    echo -e "\e[4;32m   YouTube: https://www.youtube.com/c/SpeedXTech \e[0m"
+    echo -e "\e[1;32m           facebook https://facebook.com/mahfuz038"
+    echo -e "\e[4;32m           whatsapp sorry i cant provide my personal number"
     echo " "
 
 }
@@ -126,7 +126,7 @@ do
         echo -e "\e[1;34m Downloading Latest Files..."
         rm -f .update
         $PYTHON bomber.py --update
-        echo -e "\e[1;34m RUN TBomb Again..."
+        echo -e "\e[1;34m RUN Mansmash Again..."
         pause
         exit
     elif [ $ch -eq 5 ];then
